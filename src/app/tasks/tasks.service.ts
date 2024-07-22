47b98a6e5b44d010d18ce12b1d15decd46f9ddad
rutv1k46 <rutviksolanki01@gmail.com>
2024-07-22 17:19:48 -0400

    add new tasks

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -2,17 +2,18 @@ import { Injectable, signal } from "@angular/core";
 import { Task } from "./task.model";
 
 @Injectable({
-    providedIn:'root'
+    providedIn: "root",
 })
-export class TasksService{
-    tasks = signal<Task[]>([])
+export class TasksService {
+    private tasks = signal<Task[]>([]);
+    allTasks = this.tasks.asReadonly();
 
-    addTask(taskData: {title: string, description: string }){
+    addTask(taskData: { title: string; description: string }) {
         const newTask: Task = {
             ...taskData,
             id: Math.random.toString(),
-            status: 'OPEN'
-        }
-        this.tasks.update((oldTasks) => [...oldTasks, ])
+            status: "OPEN",
+        };
+        this.tasks.update((oldTasks) => [...oldTasks, newTask]);
     }
 }
